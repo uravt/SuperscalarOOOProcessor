025d42153eb21f339fa54e74384d57210a09f431
VincentChan22 <VincentHChan22@gmail.com>
2026-02-07 13:32:26 -0500

    Update processor.h

--- processor.h
+++ processor.h
@@ -13,60 +13,97 @@ class Processor {
         //pipeline registers
         //values got based on diagram registers in and out
         //data types based on single processor implementation
-        struct IF_ID
-        {
-            uint32_t pc;
-            uint32_t instruction;
-            string toString() const 
-            {
-                return std::format("pc: {}|instruction:{}");
-            }
-        };
 
-        struct ID_EX
+    struct IF_ID
+    {
+        uint32_t pc;
+        uint32_t instruction;
+
+        std::string toString() const
         {
-            uint32_t pc;
-            uint32_t read_data_1;
-            uint32_t read_data_2;
-            uint32_t imm; //instruction[15-0]
-            int rt; //instruction[20-16]
-            int rd; //instruction[15-11]
-            uint32_t opcode;
-            uint32_t funct;
-            uint32_t shamt;
-            uint32_t addr;
-            control_t control;
-            string toString() const 
-            {
-                return std::format("pc: {}|read_data_1:{}|read_data_2{}|imm{}|rt{}|rd{}|opcode{}|funct{}|shamt{}|control{}", pc, read_data_1, read_data_2, imm, rt, rd, opcode, funct, sham, control);
-            }
-        };
-        struct EX_MEM
+            std::ostringstream oss;
+            oss << "pc:" << pc
+                << "|instruction:" << instruction;
+            return oss.str();
+        }
+    };
+
+    struct ID_EX
+    {
+        uint32_t pc;
+        uint32_t read_data_1;
+        uint32_t read_data_2;
+        uint32_t imm;     // instruction[15-0]
+        int rt;           // instruction[20-16]
+        int rd;           // instruction[15-11]
+        uint32_t opcode;
+        uint32_t funct;
+        uint32_t shamt;
+        uint32_t addr;
+        control_t control;
+
+        std::string toString() const
         {
-            uint32_t branch_target; //NOTE: not found in single impentation?? 
-            uint32_t alu_zero;
-            uint32_t alu_out;
-            uint32_t write_data_mem;
-            int write_reg;
-            uint32_t addr;
-            uint32_t branch_reg;
-            control_t control;
-            string toString() const
-            {
-                return std::format("branch_target{}|alu_zero{}|alu_out{}|write_data_mem{}|write_reg{}|control{}", branch_target, alu_zero, alu_out, write_data_mem, write_reg, control)
-            }
-        };
-        struct MEM_WB
+            std::ostringstream oss;
+            oss << "pc:" << pc
+                << "|read_data_1:" << read_data_1
+                << "|read_data_2:" << read_data_2
+                << "|imm:" << imm
+                << "|rt:" << rt
+                << "|rd:" << rd
+                << "|opcode:" << opcode
+                << "|funct:" << funct
+                << "|shamt:" << shamt
+                << "|addr:" << addr
+                << "|control:" << control;
+            return oss.str();
+        }
+    };
+
+    struct EX_MEM
+    {
+        uint32_t branch_target;
+        uint32_t alu_zero;
+        uint32_t alu_out;
+        uint32_t write_data_mem;
+        int write_reg;
+        uint32_t addr;
+        uint32_t branch_reg;
+        control_t control;
+
+        std::string toString() const
         {
-            uint32_t read_data_mem;
-            uint32_t alu_out;
-            int write_reg;
-            control_t control;
-            string toString() const
-            {
-                return std::formot("read_data_mem{}|alu_out{}|write_reg{}|control{}", read_data_mem, alu_out, write_reg, control)
-            }
-        };
+            std::ostringstream oss;
+            oss << "branch_target:" << branch_target
+                << "|alu_zero:" << alu_zero
+                << "|alu_out:" << alu_out
+                << "|write_data_mem:" << write_data_mem
+                << "|write_reg:" << write_reg
+                << "|addr:" << addr
+                << "|branch_reg:" << branch_reg
+                << "|control:" << control;
+            return oss.str();
+        }
+    };
+
+    struct MEM_WB
+    {
+        uint32_t read_data_mem;
+        uint32_t alu_out;
+        int write_reg;
+        control_t control;
+
+        std::string toString() const
+        {
+            std::ostringstream oss;
+            oss << "read_data_mem:" << read_data_mem
+                << "|alu_out:" << alu_out
+                << "|write_reg:" << write_reg
+                << "|control:" << control;
+            return oss.str();
+        }
+    };
+
 
         //two structs for reg in and reg out
         //set reg out = reg in when stepping. dont set when stalling
