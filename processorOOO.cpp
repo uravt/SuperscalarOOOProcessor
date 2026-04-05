10357236ab34bbba63f025ecdfa5ca2b33deaa07
Vincent <VincentHChan22@gmail.com>
2026-04-05 12:53:02 -0400

    wrote skeleton code for OOO setup

---
+++ processorOOO.cpp
@@ -0,0 +1,42 @@
+#include <cstdint>
+#include <cstring>
+#include <cstdio>
+#include <iostream>
+#include "processor.h"
+
+using namespace std;
+
+#define ENABLE_DEBUG
+#ifdef ENABLE_DEBUG
+#define DEBUG(x) x
+#else
+#define DEBUG(x) 
+#endif
+
+void ProcessorOOO::initialize(int level) {
+    // Initialize Control
+    control = {.reg_dest = 0, 
+               .jump = 0,
+               .jump_reg = 0,
+               .link = 0,
+               .shift = 0,
+               .branch = 0,
+               .bne = 0,
+               .mem_read = 0,
+               .mem_to_reg = 0,
+               .ALU_op = 0,
+               .mem_write = 0,
+               .halfword = 0,
+               .byte = 0,
+               .ALU_src = 0,
+               .reg_write = 0,
+               .zero_extend = 0};
+   
+    opt_level = level;
+    // Optimization level-specific initialization
+}
+
+void ProcessorOOO::advance() {
+    
+};
+
